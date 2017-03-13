2e1cc5bb10a7456415293ce48cb176e9240879af
twinh <twinhuang@qq.com>
2017-03-13 09:50:49 +0800

    解决seq构建问题 closes #3153

--- public/js/seq.js
+++ public/js/seq.js
@@ -1,15 +1,15 @@
 define(function () {
-    $.seq = function() {
-        // 优化, 允许后台输出一定数量的ID,待用完时,再次向后台申请一批ID,减少请求,提高速度
-        var id = null;
-        $.ajax({
-            async: false,
-            url: $.url('admin/seq/create'),
-            dataType: 'json',
-            success: function(result) {
-                id = result.id;
-            }
-        });
-        return id;
-    };
+  $.seq = function () {
+    // 优化, 允许后台输出一定数量的ID,待用完时,再次向后台申请一批ID,减少请求,提高速度
+    var id = null;
+    $.ajax({
+      async: false,
+      url: $.url('admin/seq/create'),
+      dataType: 'json',
+      success: function (result) {
+        id = result.id;
+      }
+    });
+    return id;
+  };
 });
