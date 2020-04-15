958ec9235678a7c838f09681f8c0bc657af3fff3
twinh <twinhuang@qq.com>
2020-04-15 10:39:30 +0800

    chore: 增加代码提示文件

---
+++ docs/auto-completion-dynamic.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Miaoxing\Seq\Service;
+
+class Seq extends \Miaoxing\Plugin\BaseService
+{
+    /**
+     * @param int $offset
+     * @return int|null
+     * @throws \Exception
+     * @see Seq::generate
+     */
+    public function generate($offset = 100)
+    {
+    }
+}
