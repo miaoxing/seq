d0a770ca46d1d681dc9320d46791304f3bed8442
twinh <twinhuang@qq.com>
2020-12-08 15:26:22 +0800

    refactor: 同步 Model 的更改

--- tests/Fixture/TestSeqTraitModel.php
+++ tests/Fixture/TestSeqTraitModel.php
@@ -2,14 +2,16 @@
 
 namespace MiaoxingTest\Seq\Fixture;
 
-use Miaoxing\Plugin\Service\Model;
+use Miaoxing\Plugin\Model\ModelTrait;
+use Miaoxing\Plugin\Service\WeiBaseModel;
 use Miaoxing\Seq\Model\SeqTrait;
 
 /**
  * @property int|null $id
  */
-class TestSeqTraitModel extends Model
+class TestSeqTraitModel extends WeiBaseModel
 {
+    use ModelTrait;
     use SeqTrait;
 
     protected $guarded = [];
